4d597b6121f504ba3c69cda45f3629249478cc03
zhang <123@qq.com>
2019-08-09 21:37:57 +0800

    架构第一次提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,11 @@
+package com.qf;/*
+* @Author ClamJohn
+  @Date 2019/8/9 
+  @Version 1.0
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
